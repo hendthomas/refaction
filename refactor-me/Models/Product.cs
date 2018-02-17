80a2ed4cb8b684622d5e61f7b3c71b9f1ff07d80
Thomas Henderson <hendthomas@gmail.com>
2018-02-17 20:48:43 +1300

    Add new Model files

---
+++ refactor-me/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace refactor_me.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public decimal DeliveryPrice { get; set; }
+    }
+}
---
+++ refactor-me/Models/ProductOption.cs
@@ -0,0 +1,26 @@
+﻿namespace refactor_me.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ProductOption")]
+    public partial class ProductOption
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
